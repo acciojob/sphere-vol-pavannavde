a52c21b4660ed24b2b5baffe19421401a6437ee0
Pavan Navde <136722639+pavannavde@users.noreply.github.com>
2023-08-18 13:17:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let ra = document.getElementById("radius");
+	let r=ra.value;
+	let volume=(4/3)*3.142*r*r*r;
 function volume_sphere() {
     //Write your code here
+	
+	let v=document.getElementById("volume");
+	v.value=volume;
   
 } 
 
